f342a64289a0cbdfcbeec608ec1ced8307d9c6ba
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2022-08-31 19:30:28 +0300

    for Vitali

---
+++ house_finder.py
@@ -0,0 +1,24 @@
+# id 69865000
+
+from typing import List
+
+
+def house_finder(length: int, houses: List[str]) -> List[int]:
+    result = [0] * length
+    nulls = [index for index, house in enumerate(houses) if house == '0']
+    first_null = nulls[0]
+    for house_index in range(0, first_null):
+        result[house_index] = first_null - house_index
+    for previous_house, current_house in zip(nulls, nulls[1:]):
+        for house_index in range(previous_house + 1, current_house):
+            result[house_index] = min(
+                house_index - previous_house, current_house - house_index
+            )
+    last_null = nulls[-1]
+    for house_index in range(last_null + 1, length):
+        result[house_index] = house_index - last_null
+    return result
+
+
+if __name__ == '__main__':
+    print(*house_finder(int(input()), input().split()))
---
+++ scores_calculation.py
@@ -0,0 +1,21 @@
+# id 69887744
+from collections import Counter
+
+NUMBER_OF_PLAYERS = 2
+SYMBOLS = '123456789'
+
+
+def scores_calculation(keys: int, template: str) -> int:
+    return sum(True
+               for symbol, amount in Counter(template).items()
+               if symbol in SYMBOLS and amount <= keys
+               )
+
+
+if __name__ == "__main__":
+    print(
+        scores_calculation(
+            keys=int(input()) * NUMBER_OF_PLAYERS,
+            template=f'{input()}{input()}{input()}{input()}',
+        )
+    )
